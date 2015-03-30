e8a13023bb6aee048a411a01bf90048ddd737d9e
Ryan Gribble <ryan.gribble@gmail.com>
2015-03-30 21:01:50 +1000

    bump version to 0.5.3

--- src/clientversion.h
+++ src/clientversion.h
@@ -21,5 +21,5 @@
 
 inline CStdString PVRWMC_GetClientVersion()
 {
-	return "0.5.2";	// ALSO CHANGE IN REV NUMBER in 'pvr.wmc/addon.xml' 
+	return "0.5.3";	// ALSO CHANGE IN REV NUMBER in 'pvr.wmc/addon.xml' 
 }
